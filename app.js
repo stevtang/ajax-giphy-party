5a99657c32207bd9cf553cfe4b40f3dda9c391aa
= <stevenmtang1@gmail.com>
2022-01-18 17:17:02 -0800

    completed clear gif function, complted append gif function, docstrings

--- app.js
+++ app.js
@@ -1,32 +1,41 @@
 "use strict";
-
+/** Evoked on form submit, retrieves form input string
+ * calls giphySearch passing input string
+ */
 function getSearchVal(e) {
   e.preventDefault();
   let formInput = $("input").val();
   console.log(formInput);
   giphySearch(formInput);
 }
-
+/** Generates and returns random number(0-50)
+ *  used as an index of JSON data
+ */
 function randomNumberGenerator() {
   return Math.floor(Math.random() * 50);
 }
-
+/** Clears gif-container div of child divs */
+function clearGifContainer() {
+  $(".gif-container").empty();
+}
+/** Accepts form input data string and passes it into axios.get request
+ * to the giphy api
+ * Takes response object and select random index of search result to
+ * append new gif to DOM
+ */
 async function giphySearch(formInput) {
-  // put formInput into ajax .get() request
-  // returns JS object
-  // access payload with .data and update DOM
-
+  let randomNumber = randomNumberGenerator();
   let giphy = await axios.get(
     `http://api.giphy.com/v1/gifs/search?q=${formInput}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`
   );
-  console.log("giphy: ", giphy);
 
-  console.log("giphy.data: ", giphy.data.data[0].url);
-
-  $(".gif-container").append($("<img>").attr("src", `${giphy.data.data[0].url}`));
-
+  $(".gif-container").append(
+    $("<img>").attr(
+      "src",
+      `${giphy.data.data[randomNumber].images.downsized_medium.url}`
+    )
+  );
 }
 
 $(".submit-search-btn").on("click", getSearchVal);
-
-console.log("Let's get this party started!");
+$(".clear-all-btn").on("click", clearGifContainer);
