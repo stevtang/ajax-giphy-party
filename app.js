5b82d3569b5aada38d2a5fd99b269c06fb256a48
Peter Loft <peterloft@Peters-MacBook-Pro.local>
2022-01-18 18:14:26 -0600

    Created first function, psuedo-coded second

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 "use strict";
 
 
+
+function getSearchVal(e) {
+    e.preventDefault();
+    let formInput = $("input").val();
+    console.log(formInput);
+    
+}
+
+function giphySearch(formInput) {
+    // put formInput into ajax .get() request
+    // returns JS object
+    // access payload with .data and update DOM
+}
+
+$(".submit-search-btn").on("click", getSearchVal);
+
 console.log("Let's get this party started!");
