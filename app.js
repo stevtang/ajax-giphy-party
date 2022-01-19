19a1570b7ae5d0ae1b85fcebb6191068a4e9af69
= <stevenmtang1@gmail.com>
2022-01-18 16:29:12 -0800

    added axios.get to log JSON, random number generator for index

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 "use strict";
 
-
-
 function getSearchVal(e) {
-    e.preventDefault();
-    let formInput = $("input").val();
-    console.log(formInput);
-    
+  e.preventDefault();
+  let formInput = $("input").val();
+  console.log(formInput);
+  giphySearch(formInput);
+}
+function randomNumberGenerator() {
+  return Math.floor(Math.random() * 50);
 }
 
-function giphySearch(formInput) {
-    // put formInput into ajax .get() request
-    // returns JS object
-    // access payload with .data and update DOM
+async function giphySearch(formInput) {
+  // put formInput into ajax .get() request
+  // returns JS object
+  // access payload with .data and update DOM
+
+  let giphy = await axios.get(
+    `http://api.giphy.com/v1/gifs/search?q=${formInput}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`
+  );
+  console.log("giphy: ", giphy);
 }
 
 $(".submit-search-btn").on("click", getSearchVal);
